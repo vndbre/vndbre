5b93ab724be146da363b433357d0dd65050a3e90
Aleksey Panov <aleksey.panov@saritasa.com>
2021-09-06 13:46:14 +0700

    chore: add  base theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App';
+import './theme/index.css';
 
 ReactDOM.render(
   <StrictMode>
