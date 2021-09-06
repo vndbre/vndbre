17788cdb4ed27b9a9250df5651f89c0021b4f071
Aleksey Panov <aleksey.panov@saritasa.com>
2021-09-06 13:56:43 +0700

    chore: add  chakra ui

--- src/features/Test/pages/TestPage/TestPage.tsx
+++ src/features/Test/pages/TestPage/TestPage.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Stack, Button } from '@chakra-ui/react';
 
 /**
  * TODO: add comments.
@@ -6,5 +7,19 @@ import React, { FC } from 'react';
 export const TestPage: FC = () => (
   <div>
     TEST PAGE
+    <Stack direction="row" spacing={4} align="center">
+      <Button colorScheme="telegram" variant="solid">
+        Button
+      </Button>
+      <Button colorScheme="teal" variant="outline">
+        Button
+      </Button>
+      <Button colorScheme="teal" variant="ghost">
+        Button
+      </Button>
+      <Button colorScheme="teal" variant="link">
+        Button
+      </Button>
+    </Stack>
   </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './App';
 import './theme/index.css';
 
 ReactDOM.render(
   <StrictMode>
     <Router>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Router>
   </StrictMode>,
   document.getElementById('root'),
