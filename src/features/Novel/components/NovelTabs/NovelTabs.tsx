5441e63e640d6e088e6e30df6f535f67b4377970
Aleksey Panov <aleksey.panov@saritasa.com>
2021-09-08 11:38:01 +0700

    refactor: reduce code duplication of NovelTabs component #13

--- src/features/Novel/components/NovelTabs/NovelTabs.tsx
+++ src/features/Novel/components/NovelTabs/NovelTabs.tsx
@@ -1,6 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, memo } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './NovelTabs.module.css';
+
+const tabNames = [
+  'Releases',
+  'Characters',
+  'Relations',
+  'Discussions',
+  'Media',
+];
 
 /**
  * TODO: add comments.
@@ -16,58 +24,39 @@ interface NovelTabsProps {
 /**
  * TODO: add comments.
  */
-export const NovelTabs: FC<NovelTabsProps> = ({ id }) => (
-  <nav>
-    <ul className={styles.list}>
-      <li>
-        <NavLink
-          to={`/vn/${id}`}
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-          end
-        >
-          Overview
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/vn/${id}/releases`}
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Releases
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/vn/${id}/characters`}
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Characters
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/vn/${id}/relations`}
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Relations
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/vn/${id}/discussions`}
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Discussions
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to={`/vn/${id}/media`}
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
-          Media
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+export const NovelTabs: FC<NovelTabsProps> = memo(({ id }) => {
+  /**
+   * TODO: Add comments.
+   * @param isActive Da.
+   */
+  const getNavLinkClassName = (isActive: boolean): string =>
+    (isActive ? `${styles.item} ${styles.active}` : styles.item);
+
+  const subroutes = tabNames.map(tabName => (
+    <li>
+      <NavLink
+        to={`/vn/${id}/${tabName.toLowerCase()}`}
+        className={({ isActive }) => getNavLinkClassName(isActive)}
+      >
+        {tabName}
+      </NavLink>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <NavLink
+            to={`/vn/${id}`}
+            className={({ isActive }) => getNavLinkClassName(isActive)}
+            end
+          >
+            Overview
+          </NavLink>
+        </li>
+        {subroutes}
+      </ul>
+    </nav>
+  );
+});
