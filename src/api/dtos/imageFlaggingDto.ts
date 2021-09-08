62f558e32b20739d855db7686e64a0affa8ee6f3
Timofei Maximov <timofei.maximov@saritasa.com>
2021-09-08 09:14:00 +0700

    feat: add vn dto

---
+++ src/api/dtos/imageFlaggingDto.ts
@@ -0,0 +1,19 @@
+/**
+ * Represents image flagging summary.
+ */
+export interface ImageFlaggingDto {
+  /** 
+   * Number of flagging votes.
+   */
+  votecount: number;
+
+  /**
+   * Sexual score between 0 (safe) and 2 (explicit).
+   */
+  sexual_avg: number | null;
+
+  /**
+   * Violence score between 0 (tame) and 2 (brutal).
+   */
+  violence_avg: number | null;
+}
---
+++ src/utils/types/links.ts
@@ -0,0 +1,14 @@
+/**
+ * Links to external resources.
+ */
+export interface Links {
+  /**
+   * Name of the related article on the English Wikipedia
+   */
+  wikipedia: string | null;
+
+  /**
+   * Wikidata identifier.
+   */
+  wikidata: string | null;
+}
