ec6c9b35072138d4cc67b048568cc4426e3024c6
moeta <teqqmu@gmail.com>
2021-11-10 13:42:43 +0700

    refactor: add memo, create function for conertion to pixels

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, memo } from 'react';
 import { Icon as IconifyIcon } from '@iconify/react';
 
 interface Props {
@@ -19,46 +19,43 @@ interface Props {
   className?: string;
 
   /**
-   * Size in pixels.
+   * Size in string format or pixels.
    */
-  size?: number | typeof stringSizes[number];
+  size?: number | keyof typeof sizes;
 }
 
-const defaultProps = {
-  style: undefined,
-  className: undefined,
-  size: 'md' as const,
+const sizes = {
+  md: 24,
+  sm: 20,
+  xs: 16,
 };
 
-const stringSizes = ['md', 'sm', 'xs'] as const;
+/**
+ * Map string sizes to number of pixels.
+ */
+function mapToPx(size: string | number): number {
+  if (typeof size === 'number') {
+    return size;
+  }
+  if (size in sizes) {
+    return sizes[size as keyof typeof sizes];
+  }
+  return sizes.md;
+}
 
 /**
- * TODO: Icon.
+ * Icon.
  */
-export const Icon: FC<Props> = ({ name, style, className, size }) => {
-  let _size = 24;
-  if (typeof size === 'string' && stringSizes.includes(size!)) {
-    switch (size) {
-      case 'sm':
-        _size = 20;
-        break;
-      case 'xs':
-        _size = 16;
-        break;
-      default:
-          _size = 24;
-    }
-  }
+export const Icon: FC<Props> = memo(({ name, style, className, size = 'md' }) => {
+  const sizePx = mapToPx(size);
 
   const mappedProps = {
     style,
     className,
     icon: name,
-    width: _size,
-    height: _size,
+    width: sizePx,
+    height: sizePx,
   };
 
   return <IconifyIcon {...mappedProps} />;
-};
-
-Icon.defaultProps = defaultProps;
+});
