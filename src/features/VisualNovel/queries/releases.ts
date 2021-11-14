f2237d0a759140fd27c3eef41436296218092606
Alexey Panov <lexade1802@gmail.com>
2021-11-14 20:59:06 +0700

    Merge branch 'feature/4-overview-tab' of github.com:vndbre/vndbre into feature/4-overview-tab

--- src/features/VisualNovel/queries/releases.ts
+++ src/features/VisualNovel/queries/releases.ts
@@ -6,6 +6,7 @@ import { Release } from '../../../models/release';
 /**
  * Custom hook for fetching releases query.
  * @param id Id of visual novel.
+ * @param options Query options.
  */
 export const useReleasesQuery = (id: string, options?: UseQueryOptions<Release[]>): UseQueryResult<Release[]> =>
   useQuery(['releases', id], () => fetchFullReleases(id), { staleTime: defaultStaleTime, ...options });
