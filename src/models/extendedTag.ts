e9caad03ff28930dff79cbe912b4e714db13d773
moeta <teqqmu@gmail.com>
2021-12-06 11:10:32 +0700

    refactor: remove code duplication

--- src/models/extendedTag.ts
+++ src/models/extendedTag.ts
@@ -1,63 +1,7 @@
-import { SpoilerLevel } from '../utils/types/spoilerLevel';
-import { TagClassification } from '../utils/types/tagClassification';
+import { Tag } from './tag';
+import { VisualNovelTag } from './visualNovel';
 
 /**
- * Represents vn tag.
+ * Represents vn tag with all possible properties.
  */
-export interface ExtendedTag {
-
-  /**
-   * Tag id.
-   */
-  readonly id: number;
-
-  /**
-   * Tag name.
-   */
-  readonly name: string;
-
-  /**
-   * (Possibly empty) list of alternative names.
-   */
-  readonly aliases: string[];
-
-  /**
-   * Tag classification.
-   */
-  readonly cat: TagClassification;
-
-  /**
-   * Whether this is a meta tag or not. This field only exists for backwards compatibility and is currently the inverse of "searchable".
-   */
-  readonly isMeta: boolean;
-
-  /**
-   * List of parent tags (empty for root tags). The first element in this array points to the primary parent tag.
-   */
-  readonly parents: number[];
-
-  /**
-   * Whether it's possible to filter VNs by this tag.
-   */
-  readonly isSearchable: boolean;
-
-  /**
-   * Number of tagged VNs (including child tags).
-   */
-  readonly visualNovelsCount: number;
-
-  /**
-   * Whether this tag can be applied to VN entries.
-   */
-  readonly isApplicable: boolean;
-
-  /**
-   * Tag score (between 1 and 3).
-   */
-  readonly score: number;
-
-  /**
-   * Spoiler level (integer, 0=major, 1=minor, 2=none).
-   */
-  readonly spoilerLevel: SpoilerLevel;
-}
+export interface ExtendedTag extends Tag, Readonly<Omit<VisualNovelTag, 'id'>> {}
