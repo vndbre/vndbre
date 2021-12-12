3e19f5ae835375c0aeb988b946dac1807b76e184
Alexey Panov <lexade1802@gmail.com>
2021-12-13 02:09:51 +0700

    feature: implement date and country services #5

---
+++ src/api/services/countryService.ts
@@ -0,0 +1,69 @@
+import { Release } from '../../models/release';
+
+/** Country. */
+export enum Country {
+  Russia = 'ru',
+  UnitedKingdom = 'en',
+  France = 'fr',
+  Italy = 'it',
+  Japan = 'ja',
+  SouthKorea = 'ko',
+  Vietnam = 'vi',
+  China = 'zh',
+  Spain = 'es',
+}
+
+interface CountryInfo {
+
+  /** Country's name. */
+  name: string;
+
+  /** Country's language. */
+  language: string;
+}
+
+export namespace CountryService {
+
+  const COUNTRY_ICON_BASE = 'twemoji:flag-for-flag-';
+
+  const MAP_COUNTRY_INFO: Record<Country, CountryInfo> = {
+    [Country.Russia]: { name: 'Russia', language: 'Russian' },
+    [Country.UnitedKingdom]: { name: 'United Kingdom', language: 'English' },
+    [Country.France]: { name: 'France', language: 'French' },
+    [Country.Italy]: { name: 'Italy', language: 'Italian' },
+    [Country.Japan]: { name: 'Japan', language: 'Japanese' },
+    [Country.SouthKorea]: { name: 'South Korea', language: 'Korean' },
+    [Country.Vietnam]: { name: 'Vietnam', language: 'Vietnamese' },
+    [Country.China]: { name: 'China', language: 'Chinese' },
+    [Country.Spain]: { name: 'Spain', language: 'Spanish' },
+  };
+
+  /**
+   * Converts a certain country to readable equivalent.
+   * @param value Country.
+   */
+  export const toReadable = (value: Country): string => MAP_COUNTRY_INFO[value]?.name ?? '';
+
+  /**
+   * Gets language spoken in country.
+   * @param value Country.
+   */
+  export const getLanguage = (value: Country): string => MAP_COUNTRY_INFO[value]?.language ?? '';
+
+  /**
+   * Gets name of icon for country.
+   * @param value Country.
+   */
+  export const getCountryIcon = (value: Country): string => COUNTRY_ICON_BASE + toReadable(value)
+    .toLowerCase()
+    .split(' ')
+    .join('-');
+
+  /**
+   * Gets all unique countries from releases.
+   * @param releases Releases of visual novel.
+   */
+  export const getCountriesFromReleases = (releases: Release[]): Country[] => Array.from(
+    new Set(releases.map(release => release.languages).flat()),
+  ) as Country[];
+}
