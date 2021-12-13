437f3bafc5cbf2d90a1be17e5b6e2ae207fe3168
moeta <teqqmu@gmail.com>
2021-12-13 17:50:57 +0700

    fix: use proper link component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Link } from '@chakra-ui/react';
 import React, { memo, VFC } from 'react';
+import { NavLink } from 'react-router-dom';
 import cls from './Sidebar.module.css';
 
 /** Sidebar element type. */
@@ -111,12 +112,12 @@ export const Sidebar: VFC = memo(() => (
           return (
             <>
               <Heading className={cls.heading}>{el.text}</Heading>
-              {el.items.map(_el => <a href={_el.link} className={cls.link}>{_el.text}</a>)}
+              {el.items.map(_el => <Link as={NavLink} to={_el.link}>{_el.text}</Link>)}
             </>
           );
         }
         case SidebarElementType.Link: {
-          return <a href={el.link} className={cls.link}>{el.text}</a>;
+          return <Link as={NavLink} to={el.link}>{el.text}</Link>;
         }
         default: {
           return null;
