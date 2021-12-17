d5911ed24fe9db423e5545923f58f99fde6c2d73
Vladislav Prekel <misterptits@yandex.ru>
2021-12-17 15:33:15 +0700

    fix: styles for BBCode component

--- src/components/BBCode/BBCode.tsx
+++ src/components/BBCode/BBCode.tsx
@@ -7,11 +7,14 @@ import BBCodeUntyped from '@bbob/react/es/Component';
 // @ts-ignore
 import reactPreset from '@bbob/preset-react/es';
 
+import cls from './BBCode.module.css';
+
 /**
  * Typed BBCode component.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const BBCode: FC = ({ children }) => (
-  <BBCodeUntyped plugins={reactPreset()}>
+  <BBCodeUntyped plugins={reactPreset()} componentProps={{ className: cls.bbcode }}>
     {children}
   </BBCodeUntyped>
 );
