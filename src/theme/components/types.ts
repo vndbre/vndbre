c0fc00610f8d99726b7a211d4a0da91e0a4858d9
moeta <teqqmu@gmail.com>
2021-12-26 14:24:20 +0700

    refactor: add size type

--- src/theme/components/types.ts
+++ src/theme/components/types.ts
@@ -1,20 +1,22 @@
 /* eslint-disable jsdoc/require-jsdoc */
 import { SystemStyleObject, Theme } from '@chakra-ui/react';
 
-type StyleInterpolation = SystemStyleObject | ((options: StyleOptions) => SystemStyleObject);
-
 interface StyleOptions {
   theme: Theme;
   colorMode: 'light' | 'dark';
   colorScheme: string;
 }
 
+type StyleInterpolation = SystemStyleObject | ((options: StyleOptions) => SystemStyleObject);
+
+type Size = 'xs' | 'sm' | 'base' | 'md' | 'lg' | 'xl';
+
 export interface StyleConfig {
   baseStyle?: StyleInterpolation;
-  sizes?: Record<string, StyleInterpolation>;
+  sizes?: Record<Size, StyleInterpolation>;
   variants?: Record<string, StyleInterpolation>;
   defaultProps?: {
     variant: string;
-    size: string;
+    size: Size;
   };
 }
