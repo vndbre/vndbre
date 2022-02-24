35897ee5eea3a77300febe9f5b9c7129c3f85119
moetazaneta <teqqmu@gmail.com>
2022-02-24 10:52:03 +0700

    refactor: simplify useLocalStorage #99

--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -1,40 +1,24 @@
 import { useState } from 'react';
 
-/** Sync state to local storage so that it persists through a page refresh. */
+/**
+ * Sync state to local storage so that it persists through a page refresh.
+ * @param key Local store key.
+ * @param initialValue Initial value, returned on errors.
+ */
 export function useLocalStorage<T>(key: string, initialValue: T): [T, ((value: T) => void)] {
-  // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
-    if (typeof window === 'undefined') {
-      return initialValue;
-    }
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-
-      // Parse stored json or if none return initialValue
-      return item ? (JSON.parse(item) as T) : initialValue;
-    } catch (error: unknown) {
-      // If error also return initialValue
-      return initialValue;
-    }
+    const item = window.localStorage.getItem(key);
+    return item ? (JSON.parse(item) as T) : initialValue;
   });
 
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
-  /** Value setter function. */
+  /**
+   * Local storage value setter.
+   * @param value Value.
+   */
   const setValue = (value: T): void => {
-    try {
-      // Save state
-      setStoredValue(value);
-
-      // Save to local storage
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(value));
-      }
-    } catch (error: unknown) {
-      // A more advanced implementation would handle the error case
-    }
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
   };
   return [storedValue, setValue];
 }
