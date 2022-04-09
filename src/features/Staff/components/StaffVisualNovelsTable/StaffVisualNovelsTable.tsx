150e19ee87c616f50c8f1764307cc6c30ba58440
Timofey Maximov <moltenship7750@gmail.com>
2022-04-09 20:22:05 +0700

    fix: simplify template, move out interface to model #80

--- src/features/Staff/components/StaffVisualNovelsTable/StaffVisualNovelsTable.tsx
+++ src/features/Staff/components/StaffVisualNovelsTable/StaffVisualNovelsTable.tsx
@@ -1,31 +1,39 @@
-import React, { VFC, memo, useMemo } from 'react';
+import React, { VFC, memo } from 'react';
 import { Link, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 import { DateService } from '../../../../api/services/dateService';
 import { StaffVisualNovel } from '../../../../models/staff';
 import { StaffRole } from '../../../../models/staffRole';
 import { VisualNovel } from '../../../../models/visualNovels/visualNovel';
+import { StaffExtendedVisualNovel } from '../../../../models/staffExtendedVisualNovel';
 
 interface Props {
 
   /** List of visual novels that the the staff is involved. */
   readonly visualNovels: readonly VisualNovel[];
 
-  /** List of roles and vn ids the staff involved involved. */
+  /** List of roles and vn ids the staff is involved. */
   readonly staffVisualNovels: readonly StaffVisualNovel[];
 }
 
-interface ExtendedVisualNovel extends VisualNovel, StaffVisualNovel {}
-
 /** Staff visual novels components. */
 const StaffVisualNovelsTableComponent: VFC<Props> = ({ visualNovels, staffVisualNovels }) => {
-  const groupedVisualNovels = useMemo(() => staffVisualNovels.reduce<ExtendedVisualNovel[]>((acc, cur) => {
+  const groupedVisualNovels = staffVisualNovels.reduce<StaffExtendedVisualNovel[]>((acc, cur) => {
     const novel = visualNovels.find(vn => vn.id === cur.id);
     if (novel) {
       return [...acc, { ...novel, ...cur }];
     }
     return acc;
-  }, []), [visualNovels, staffVisualNovels]);
+  }, []);
+
+  const tableRow = groupedVisualNovels.map(vn => (
+    <Tr key={`${vn.id + vn.aliasId}${vn.note}`}>
+      <Td whiteSpace="pre-wrap"><Link as={NavLink} to={`/vn/${vn.id}`}>{vn.title}</Link></Td>
+      <Td>{vn.released ? DateService.toISODate(vn.released) : 'No info'}</Td>
+      <Td>{StaffRole.toReadable(vn.role)}</Td>
+      <Td whiteSpace="pre-wrap">{vn.note}</Td>
+    </Tr>
+  ));
 
   return (
     <TableContainer>
@@ -39,14 +47,7 @@ const StaffVisualNovelsTableComponent: VFC<Props> = ({ visualNovels, staffVisual
           </Tr>
         </Thead>
         <Tbody>
-          {groupedVisualNovels.map(vn => (
-            <Tr key={`${vn.id + vn.aliasId}${vn.note}`}>
-              <Td whiteSpace="pre-wrap"><Link as={NavLink} to={`/vn/${vn.id}`}>{vn.title}</Link></Td>
-              <Td>{vn.released ? DateService.toISODate(vn.released) : 'No info'}</Td>
-              <Td>{StaffRole.toReadable(vn.role)}</Td>
-              <Td whiteSpace="pre-wrap">{vn.note}</Td>
-            </Tr>
-          ))}
+          {tableRow}
         </Tbody>
       </Table>
     </TableContainer>
