644253d45fd8ab7f5d2d9c8ec43419b7e9518c1b
moeta <teqqmu@gmail.com>
2022-04-23 17:29:08 +0700

    fix: use proper prefix for boolean prop #115

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -11,19 +11,19 @@ interface Props {
   readonly children?: ReactNode;
 
   /** Whether loader should take full height. */
-  readonly fullHeight?: boolean;
+  readonly hasFullHeight?: boolean;
 }
 
 /**
  * Loading component.
  */
-const LoadingComponent: VFC<Props> = ({ isLoading, children, fullHeight = false }) => (
+const LoadingComponent: VFC<Props> = ({ isLoading, children, hasFullHeight = false }) => (
   <>
     {isLoading ? (
       <Box
         py={4}
         w="full"
-        h={fullHeight ? 'full' : undefined}
+        h={hasFullHeight ? 'full' : undefined}
         display="flex"
         alignItems="center"
         justifyContent="center"
