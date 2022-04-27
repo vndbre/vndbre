32aa2e7f61a7e4773466d65fa072215e4f6cee1d
moeta <teqqmu@gmail.com>
2022-04-27 21:53:53 +0700

    refactor: move out function #116

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -12,38 +12,22 @@ interface Props {
 }
 
 /**
- * Sidebar.
+ * Gets link element.
+ * @param element Sidebar link element.
+ * @returns
  */
-export const Sidebar: VFC<Props> = ({ onSidebarHide }) => {
-  /**
-   * Gets link element.
-   * @param element Sidebar link element.
-   * @returns
-   */
-  const getLink = (element: SidebarLinkElement): ReactNode => {
-    const disabledStyles = element.isDisabled ? {
-      pointerEvents: 'none',
-      color: 'gray.400',
-    } : undefined;
-    if (element.isExternal) {
-      return (
-        <Link
-          key={element.link + element.text}
-          href={element.link}
-          target="_blank"
-          variant="no-underline"
-          className={cls.link}
-          sx={disabledStyles}
-        >
-          {element.text}
-        </Link>
-      );
-    }
+const getLink = (element: SidebarLinkElement): ReactNode => {
+  const disabledStyles = element.isDisabled ? {
+    pointerEvents: 'none',
+    color: 'gray.400',
+  } : undefined;
+
+  if (element.isExternal) {
     return (
       <Link
         key={element.link + element.text}
-        as={RouterLink}
-        to={element.link}
+        href={element.link}
+        target="_blank"
         variant="no-underline"
         className={cls.link}
         sx={disabledStyles}
@@ -51,40 +35,55 @@ export const Sidebar: VFC<Props> = ({ onSidebarHide }) => {
         {element.text}
       </Link>
     );
-  };
-
+  }
   return (
-    <aside className={cls.sidebar}>
-      <HStack className={cls.logo} justifyContent="space-between" alignItems="center" flexShrink={0} height={16}>
-        <Heading as="h2" size="md">vndbre</Heading>
-        <IconButton
-          onClick={onSidebarHide}
-          aria-label="Toggle sidebar"
-          variant="ghost"
-          colorScheme="gray"
-          icon={<Icon name="carbon:close" />}
-        />
-      </HStack>
-      <Box className={cls.navigation}>
-        {data.map(el => {
-          switch (el.type) {
-            case SidebarElementType.Heading: {
-              return (
-                <Fragment key={el.text + el.type}>
-                  <Heading as="h3" size="sm" className={cls.heading}>{el.text}</Heading>
-                  {el.items.map(getLink)}
-                </Fragment>
-              );
-            }
-            case SidebarElementType.Link: {
-              return getLink(el);
-            }
-            default: {
-              return null;
-            }
-          }
-        })}
-      </Box>
-    </aside>
+    <Link
+      key={element.link + element.text}
+      as={RouterLink}
+      to={element.link}
+      variant="no-underline"
+      className={cls.link}
+      sx={disabledStyles}
+    >
+      {element.text}
+    </Link>
   );
 };
+
+/**
+ * Sidebar.
+ */
+export const Sidebar: VFC<Props> = ({ onSidebarHide }) => (
+  <aside className={cls.sidebar}>
+    <HStack className={cls.logo} justifyContent="space-between" alignItems="center" flexShrink={0} height={16}>
+      <Heading as="h2" size="md">vndbre</Heading>
+      <IconButton
+        onClick={onSidebarHide}
+        aria-label="Toggle sidebar"
+        variant="ghost"
+        colorScheme="gray"
+        icon={<Icon name="carbon:close" />}
+      />
+    </HStack>
+    <Box className={cls.navigation}>
+      {data.map(el => {
+        switch (el.type) {
+          case SidebarElementType.Heading: {
+            return (
+              <Fragment key={el.text + el.type}>
+                <Heading as="h3" size="sm" className={cls.heading}>{el.text}</Heading>
+                {el.items.map(getLink)}
+              </Fragment>
+            );
+          }
+          case SidebarElementType.Link: {
+            return getLink(el);
+          }
+          default: {
+            return null;
+          }
+        }
+      })}
+    </Box>
+  </aside>
+);
