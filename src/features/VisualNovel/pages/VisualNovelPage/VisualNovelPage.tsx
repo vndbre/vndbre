03013c549f2440e9343feefe59dbc79969cbd6e1
Timofey Maximov <moltenship7750@gmail.com>
2022-04-28 22:15:24 +0700

    feat: add `react-helmet` #123

--- src/features/VisualNovel/pages/VisualNovelPage/VisualNovelPage.tsx
+++ src/features/VisualNovel/pages/VisualNovelPage/VisualNovelPage.tsx
@@ -1,6 +1,7 @@
 import React, { FC, Suspense, useMemo } from 'react';
 import { Button, ButtonGroup, IconButton, Text } from '@chakra-ui/react';
 import { Outlet } from 'react-router';
+import { Helmet } from 'react-helmet';
 
 import { Icon } from '../../../../components/Icon/Icon';
 import cls from './VisualNovelPage.module.css';
@@ -41,46 +42,52 @@ export const VisualNovelPage: FC = () => {
   return (
     <ContentWrapper isLoading={isLoading} error={error}>
       {data && (
-        <div className={cls.page}>
-          <header className={cls.header}>
-            <div className={cls.overview}>
-              <SafeImage
-                containerProps={{
-                  borderRadius: 'lg',
-                  h: 'max-content',
-                }}
-                objectFit="cover"
-                height="auto"
-                maxH="80"
-                src={data.image}
-                fallbackSrc={vnPosterPlaceholder}
-                alt={data.title}
-                isNsfw={data.isImageNsfw}
-              />
-              <div className={cls.info}>
-                <div className={cls.heading}>
-                  <EntityTitle title={data.title} originalTitle={data.originalName} />
+        <>
+          <Helmet>
+            <title>{data.title}</title>
+            <meta name="og:image" content={data.image ?? undefined} />
+          </Helmet>
+          <div className={cls.page}>
+            <header className={cls.header}>
+              <div className={cls.overview}>
+                <SafeImage
+                  containerProps={{
+                    borderRadius: 'lg',
+                    h: 'max-content',
+                  }}
+                  objectFit="cover"
+                  height="auto"
+                  maxH="80"
+                  src={data.image}
+                  fallbackSrc={vnPosterPlaceholder}
+                  alt={data.title}
+                  isNsfw={data.isImageNsfw}
+                />
+                <div className={cls.info}>
+                  <div className={cls.heading}>
+                    <EntityTitle title={data.title} originalTitle={data.originalName} />
+                  </div>
+                  <div className={cls.controls}>
+                    <ButtonGroup isAttached>
+                      <Button mr="-px">Add to list</Button>
+                      <IconButton aria-label="Add to list extended" icon={<Icon name="carbon:chevron-down" />} />
+                    </ButtonGroup>
+                    <IconButton aria-label="Star" icon={<Icon name="carbon:star" />} colorScheme="gray" />
+                    <IconButton aria-label="Edit" icon={<Icon name="carbon:edit" />} colorScheme="gray" />
+                    <IconButton aria-label="Report" icon={<Icon name="carbon:flag" />} colorScheme="gray" />
+                  </div>
+                  {description}
                 </div>
-                <div className={cls.controls}>
-                  <ButtonGroup isAttached>
-                    <Button mr="-px">Add to list</Button>
-                    <IconButton aria-label="Add to list extended" icon={<Icon name="carbon:chevron-down" />} />
-                  </ButtonGroup>
-                  <IconButton aria-label="Star" icon={<Icon name="carbon:star" />} colorScheme="gray" />
-                  <IconButton aria-label="Edit" icon={<Icon name="carbon:edit" />} colorScheme="gray" />
-                  <IconButton aria-label="Report" icon={<Icon name="carbon:flag" />} colorScheme="gray" />
-                </div>
-                {description}
               </div>
+              <EntityTabs id={id} tabsInfo={VISUAL_NOVELS_ROUTES_INFO} entityRootPath="vn" />
+            </header>
+            <div>
+              <Suspense fallback={<Loading isLoading />}>
+                <Outlet />
+              </Suspense>
             </div>
-            <EntityTabs id={id} tabsInfo={VISUAL_NOVELS_ROUTES_INFO} entityRootPath="vn" />
-          </header>
-          <div>
-            <Suspense fallback={<Loading isLoading />}>
-              <Outlet />
-            </Suspense>
           </div>
-        </div>
+        </>
       )}
     </ContentWrapper>
   );
