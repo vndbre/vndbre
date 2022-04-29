25d9e9b55aa2da3a7474f0baf4d9f3890f32f157
moeta <teqqmu@gmail.com>
2022-04-29 11:31:56 +0700

    feat: implement first prototype #151

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, useCallback, VFC } from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { Suspense, useCallback, useState, VFC } from 'react';
+import { Box, Fade } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../components/Header/Header';
 import { Sidebar } from '../../components/Sidebar/Sidebar';
@@ -14,17 +14,37 @@ import { Loading } from '../../components';
 export const DefaultLayout: VFC = () => {
   const [isSidebarVisible, setSidebarVisibility] = useLocalStorage(KEY_IS_SIDEBAR_VISIBLE, true);
 
-  const showSidebar = useCallback(() => setSidebarVisibility(true), []);
-  const hideSidebar = useCallback(() => setSidebarVisibility(false), []);
+  const [sidebarClasses, setSidebarClasses] = useState<string[]>([]);
+  const [containerClasses, setContainerClasses] = useState<string[]>([]);
+
+  const showSidebar = useCallback(() => {
+    setSidebarClasses(['sidebar-hidden']);
+    setSidebarVisibility(true);
+    setTimeout(() => setSidebarClasses(['sidebar-visible']), 0);
+    setTimeout(() => setSidebarClasses([]), 300);
+  }, []);
+  const hideSidebar = useCallback(() => {
+    setSidebarClasses(['sidebar-visible']);
+    setTimeout(() => setSidebarClasses(['sidebar-hidden']), 0);
+    setTimeout(() => {
+      setSidebarVisibility(false);
+      setSidebarClasses([]);
+    }, 300);
+  }, []);
 
   return (
     <Box className={cls.layout}>
       {isSidebarVisible && (
-        <Box className={cls.sidebar}>
+        <Box className={[cls.sidebar, sidebarClasses.map(className => cls[className])].join(' ')}>
           <Sidebar onSidebarHide={hideSidebar} />
         </Box>
       )}
-      <Box className={`${cls.container} ${isSidebarVisible ? cls['with-sidebar'] : ''}`}>
+      <Box className={`
+        ${cls.container}
+        ${isSidebarVisible ? cls['with-sidebar'] : ''}
+        ${[sidebarClasses.map(className => cls[className])].join(' ')}
+      `}
+      >
         <Header isLogoVisible={!isSidebarVisible} onSidebarShow={showSidebar} />
         <Box className={cls.content}>
           <Suspense fallback={<Loading hasFullHeight isLoading />}>
