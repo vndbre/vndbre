eb1f62369f494990ddc822989134bb88bd0af59f
moetazaneta <teqqmu@gmail.com>
2022-11-24 02:06:16 +0700

    Add typography example VN-109

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      xs: '1rem',
+      sm: '1.125rem',
+      base: '1.25rem',
+      lg: '1.5rem',
+    },
+    colors: {
+      orange: colors.orange,
+      gray: colors.neutral,
+      white: colors.white,
+      black: colors.black,
+      red: colors.red,
+      green: colors.green,
+    }
   },
   plugins: [],
 };
