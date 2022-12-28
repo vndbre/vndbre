97c03895d0eb9f7015310cde9934fcb31b8866bd
moetazaneta <teqqmu@gmail.com>
2022-12-28 22:22:55 +0700

    fix forwardRef error

--- src/components/Tabs/TabsList.tsx
+++ src/components/Tabs/TabsList.tsx
@@ -1,20 +1,20 @@
-import type { FC } from 'react';
-import React, { memo } from 'react';
+import React, { forwardRef, memo } from 'react';
 import * as RadixTabs from '@radix-ui/react-tabs';
 import clsx from 'clsx';
 
 export type TabsListProps = RadixTabs.TabsListProps;
 
 /** Contains tabs. */
-const TabsListComponent: FC<TabsListProps> = ({
+const TabsListComponent = forwardRef<HTMLDivElement, TabsListProps>(({
   className,
   children,
   ...props
-}) => (
-  <RadixTabs.List {...props} className={clsx('relative flex w-full overflow-x-auto', className)}>
+}, ref) => (
+  <RadixTabs.List ref={ref} {...props} className={clsx('relative flex w-full overflow-x-auto', className)}>
     <div className="absolute bottom-0 left-0 -z-10 w-full border-b border-gray-200" />
     {children}
   </RadixTabs.List>
-);
+));
+TabsListComponent.displayName = 'TabsList';
 
 export const TabsList = memo(TabsListComponent);
