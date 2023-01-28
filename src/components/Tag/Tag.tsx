3689a41929cdbac0ef92a22f76fb8b3f22bc7ff4
moetazaneta <teqqmu@gmail.com>
2023-01-28 23:43:57 +0700

    Add multiline prop for tag

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -7,17 +7,26 @@ interface Props extends PropsWithChildrenAndClass {
 
   /** Left icon. */
   readonly iconLeft?: ReactNode;
+
+  /** Whether tag is multiline. */
+  readonly isMultiLine?: boolean;
 }
 
 /** Tag. */
-const TagComponent: FC<Props> = ({ children, className, iconLeft }) => (
+const TagComponent: FC<Props> = ({ children, className, iconLeft, isMultiLine }) => (
   <div
     className={clsx(
       'flex cursor-default items-center gap-1 overflow-hidden rounded bg-gray-100 px-2 py-1 text-sm leading-6', className,
     )}
   >
     {iconLeft != null && <div className="shrink-0">{iconLeft}</div>}
-    <span className="overflow-hidden text-ellipsis">{children}</span>
+    <div
+      className={clsx('overflow-hidden text-ellipsis', {
+        'line-clamp-1': !isMultiLine,
+      })}
+    >
+      {children}
+    </div>
   </div>
 );
 
