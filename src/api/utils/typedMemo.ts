e83ae585249fc39414479a79f79fee401e5437e9
Timofey Maximov <40624720+Moltenship@users.noreply.github.com>
2023-02-01 23:03:39 +0700

    Update src/api/utils/typedMemo.ts
    Co-authored-by: Alexey <lexade1802@gmail.com>

--- src/api/utils/typedMemo.ts
+++ src/api/utils/typedMemo.ts
@@ -4,6 +4,5 @@ import { memo } from 'react';
 /**
  * Typed memo.
  * @param component Component.
- * @param Component
  */
 export const typedMemo: <T extends ComponentType>(component: T) => T = memo;
