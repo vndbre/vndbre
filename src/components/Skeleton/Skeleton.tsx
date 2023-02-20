1650ccffc332511e41a977e64e260a27b1d50220
Timofey Maximov <moltenship7750@gmail.com>
2023-02-20 16:57:02 +0700

    add todo for skeleton

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -3,6 +3,7 @@ import type { FC } from 'react';
 import { memo } from 'react';
 import type { PropsWithChildrenAndClass } from 'src/utils/PropsWithClass';
 
+// TODO: Add props for text/rectangle types
 /** Skeleton component. */
 const SkeletonComponent: FC<PropsWithChildrenAndClass> = ({ children, className }) => (
   <div className={clsx('animate-pulse bg-gray-200', className)}>
