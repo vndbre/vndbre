e6a85f49a06f033d0497a23b88921943dd01c5fa
Timofey Maximov <moltenship7750@gmail.com>
2023-02-20 17:10:14 +0700

    remove id sorting

--- src/api/models/queryOptions/vn/vnSortField.ts
+++ src/api/models/queryOptions/vn/vnSortField.ts
@@ -1,11 +1,10 @@
 /** List of available sort fields for vn. */
-export const VN_SORT_FIELDS = ['id', 'title', 'released', 'popularity', 'rating', 'votecount', 'length'] as const;
+export const VN_SORT_FIELDS = ['title', 'released', 'popularity', 'rating', 'votecount', 'length'] as const;
 
 /** Sort field for vn. */
 export type VnSortField = typeof VN_SORT_FIELDS[number];
 
 const VN_SORT_FIELD_READABLE_MAP: Readonly<Record<VnSortField, string>> = {
-  id: 'Id',
   length: 'Length',
   popularity: 'Popularity',
   rating: 'Rating',
