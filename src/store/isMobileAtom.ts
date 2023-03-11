e957d90c8a5d2273f630111a909c3763c57f4544
Timofey Maximov <moltenship7750@gmail.com>
2023-03-11 18:28:36 +0700

    Fix docs

--- src/store/isMobileAtom.ts
+++ src/store/isMobileAtom.ts
@@ -2,6 +2,6 @@ import { atom, useAtom } from 'jotai';
 
 export const isMobileAtom = atom(false);
 
-/** Whether current device is mobile. */
+/** Hook for getting is current device is mobile. */
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const useIsMobile = () => useAtom(isMobileAtom)[0];
