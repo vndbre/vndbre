79f630fd5262e0bdf00812811307d9747b2ca705
Timofey Maximov <moltenship7750@gmail.com>
2023-05-05 19:43:36 +0700

    WIP

--- src/features/profile/pages/ProfileOverviewPage.tsx
+++ src/features/profile/pages/ProfileOverviewPage.tsx
@@ -1,11 +1,8 @@
 import type { FC } from 'react';
-import { Layout } from 'src/components/Layout/Layout';
 
 /** Profile overview page. */
 const ProfileOverviewPage: FC = () => (
-  <Layout>
-    Profile overview page
-  </Layout>
+  <>Profile overview page</>
 );
 
 export default ProfileOverviewPage;
