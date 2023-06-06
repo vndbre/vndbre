9e272a1265aa16b8807b5396a365ba7cf16acddc
Timofey Maximov <moltenship7750@gmail.com>
2023-06-06 22:35:46 +0700

    Remove meta viewport tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,7 @@ const RootLayout = async({ children }: PropsWithChildren) => {
 
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
+      <head />
       <body className={`${inter.variable} font-sans`}>
         <AuthProvider session={session}>
           <QueryProvider>
